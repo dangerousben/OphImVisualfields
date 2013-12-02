acaa80a163760005e921343d8e68a5fa7c5ac90e
Ben Spencer <ben.spencer@openeyes.org.uk>
2013-12-02 15:45:36 +0000

    Config.

---
+++ config/console.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * (C) OpenEyes Foundation, 2013
+ * This file is part of OpenEyes.
+ * OpenEyes is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * OpenEyes is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with OpenEyes in a file titled COPYING. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package OpenEyes
+ * @link http://www.openeyes.org.uk
+ * @author OpenEyes <info@openeyes.org.uk>
+ * @copyright Copyright (C) 2013, OpenEyes Foundation
+ * @license http://www.gnu.org/licenses/gpl-3.0.html The GNU General Public License V3.0
+ */
+
+$config = array();
+
+$dh = opendir(__DIR__ . "/../commands");
+
+while ($file = readdir($dh)) {
+	if (preg_match('/^(.*?)Command\.php$/', $file, $m)) {
+		$config['commandMap'][strtolower($m[1])] = array(
+			'class' => "application.modules.OphImVisualfields.commands.{$m[1]}Command",
+		);
+	}
+}
+
+closedir($dh);
+
+return $config;
